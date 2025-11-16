acc96d409a42e545e2aa82e101056f2950bc8862
BRMChris141 <eliothsteban@gmail.com>
2025-11-15 18:12:49 -0600

    Version 2.0 Cambio Lujan

--- src/vista/GestionProductosPOO.java
+++ src/vista/GestionProductosPOO.java
@@ -7,10 +7,15 @@ import java.util.Scanner;
 public class GestionProductosPOO {
    
     public static void main(String[] args) {
-         Scanner entradaDato= new Scanner(System.in);
+    Scanner entradaDato= new Scanner(System.in);
     GestorProductos gestor = new GestorProductos();
-        
-    System.out.println("Sistema de productos\n Captura ID:");
+    int ciclo;
+        do {            
+            System.out.println("Menu Productos\n1. Agregar\n2. Consultar\n3. Eliminar\n4. Salir\n");
+            ciclo= entradaDato.nextInt();
+            switch (ciclo) {
+                case 1: 
+                    System.out.println("Sistema de productos\n Captura ID:");
     int id= entradaDato.nextInt();
         System.out.println("Captura nombre:");
         String nombre= entradaDato.next();
@@ -20,16 +25,35 @@ public class GestionProductosPOO {
         int stock= entradaDato.nextInt();
         
         gestor.agregarPoducto(new Producto(id, nombre, precio, stock));
-    
+                    break;
+                    
+                case 2: 
         gestor.ListarProductos();
-        
-        System.out.println("ID a buscar:");
+                         break;
+                case 3: 
+                    System.out.println("ID a buscar:");
         Producto p = gestor.buscarPorID(entradaDato.nextInt());
         System.err.println(p != null ? p : "Producto no encontrado");
         System.out.println("ID a eliminar:");
         gestor.eiminarProducto(entradaDato.nextInt());
 
         gestor.ListarProductos();
+                    break;
+                case 4:
+                    System.out.println("Finalizo Programa");
+                    break;
+                
+                default: System.out.println("Opcion invalida solo tengo 1-4"); break;
+            }
+        } while (ciclo!=4);
+        
+        
+    
+    
+    
+   
+        
+        
     }
     
 }
